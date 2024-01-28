1a8b9f0de0c1279b4d4bcf96f83e4507c35de778
abdulahadabduvahobov75@gmail.com <abdulahadabduvahobov75@gmail.com>
2024-01-28 18:59:01 +0500

    go

--- src/components/TheBalolo.vue
+++ src/components/TheBalolo.vue
@@ -4,45 +4,46 @@
       <source src="../video/video1.mp4" type="video/mp4" />
       Ваш браузер не поддерживает тег video.
     </video>
-    <div class="md:py-[100px] md:px-[20px] md:w-[620px] mx-auto  md:mt-[150px] w-[360px] z-50">
-      <div>
-
-          <h2
-          class="mb-[8px] text-white text-[22px] font-semibold text-center aos-init aos-animate"
-          data-aos="fade-up"
-          data-aos-duration="1500"
-        >
-          The BALOLO Setup Cockpit
-        </h2>
-        <h3
-          class="text-white text-center text-[14px] font-normal mb-[24px] aos-init aos-animate"
-          data-aos="fade-up"
-          data-aos-duration="1500"
-        >
-          Use code 'LANDONBTW'
-        </h3>
-        <div
-          class="aos-init aos-animate"
-          data-aos="fade-up"
-          data-aos-duration="1500"
-        >
-          <a
-            href="https://www.balolo.de/en"
-            target="_blank"
-            class="py-[13px] bg-gradient-to-r cursor-pointer w-[150px] block hover:-translate-y-1 transition mx-auto from-[#373737] to-[#0E0E0E]"
-          >
-            <span class="text-white font-semibold text-center block"
-              >View Products</span
-            >
-          </a>
-        </div>
+    <div class="md:py-[100px] relative md:px-[20px] md:w-[420px] mx-auto  md:mt-[150px] w-[300px] z-50">
+      <div class="">
+          <div class="w-[100px] h-[50px] aos-init aos-animate " data-aos="fade-up-right" data-aos-duration="1500">
+            <img src="../assets/p7.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate ml-auto" data-aos="fade-up-left" data-aos-duration="1500">
+            <img src="../assets/p8.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate " data-aos="fade-up-right" data-aos-duration="1500">
+            <img src="../assets/p9.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate ml-auto" data-aos="fade-up-left" data-aos-duration="1500">
+            <img src="../assets/p10.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate " data-aos="fade-up-right" data-aos-duration="1500">
+            <img src="../assets/p6.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px]] aos-init aos-animate ml-auto" data-aos="fade-up-left" data-aos-duration="1500">
+            <img src="../assets/p5.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate " data-aos="fade-up-right" data-aos-duration="1500">
+            <img src="../assets/p4.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate ml-auto" data-aos="fade-up-left" data-aos-duration="1500">
+            <img src="../assets/p3.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate " data-aos="fade-up-right" data-aos-duration="1500">
+            <img src="../assets/p2.jpg" alt="img">
+          </div>
+          <div class="w-[100px] h-[50px] aos-init aos-animate ml-auto" data-aos="fade-up-left" data-aos-duration="1500">
+            <img src="../assets/p1.jpg" alt="img">
+          </div>
+          
       </div>
     </div>
     <div
       class="the_balolo_top md:h-[102px] h-[50px] absolute top-0 left-0 right-0 z-30"
     ></div>
     <div
-      class="the_balolo_bottom md:h-[102px] h-[50px] absolute bottom-0 left-0 right-0 z-30"
+      class="the_balolo_bottom md:h-[102px] h-[50px] absolute bottom-[-1px] left-0 right-0 z-30"
     ></div>
   </div>
 </template>
@@ -59,8 +60,8 @@ export default {};
 
 .the_balolo_container {
   position: relative;
+  height: 100%;
   width: 100%;/* или любая другая высота, которую вы хотите использовать */
-  overflow: hidden;
 }
 
 .the_balolo_container video {
