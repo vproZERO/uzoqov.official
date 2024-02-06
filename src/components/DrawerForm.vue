faf35a323d7edb44ddc75f4e2e9e49ceffbdd265
abdulahadabduvahobov75@gmail.com <abdulahadabduvahobov75@gmail.com>
2024-02-06 15:03:11 +0500

    a

--- src/components/DrawerForm.vue
+++ src/components/DrawerForm.vue
@@ -24,7 +24,7 @@
           name="tel"
           id="tel"
           class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-
+          placeholder=" "
           required
         />
         <label
